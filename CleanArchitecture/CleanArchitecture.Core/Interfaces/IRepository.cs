56d59073371f95c2f8411ca0eb6fea598ca4cba5
Michał Panfil <it.panfil@gmail.com>
2023-03-17 22:55:15 +0100

    generic repository

--- CleanArchitecture/CleanArchitecture.Core/Interfaces/IRepository.cs
+++ CleanArchitecture/CleanArchitecture.Core/Interfaces/IRepository.cs
@@ -6,9 +6,14 @@ using System.Threading.Tasks;
 
 namespace CleanArchitecture.Core.Interfaces
 {
-    public interface IRepository
+    public interface IRepository<T> where T : class
     {
         Task<T> GetById(Guid id);
-        Task<T> GetById(Predignat p);
+        Task<IQueryable<T>> Get();
+        Task<T> Add(T entity);
+        Task Delete(T entity);
+        Task Update(T entity);
+
     }
+    
 }
---
+++ CleanArchitecture/CleanArchitecture.Infrastructure/Services/GenericRepository.cs
@@ -0,0 +1,37 @@
+﻿using CleanArchitecture.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Infrastructure.Services
+{
+    public class GenericRepository<T> : IRepository<T> where T : class
+    {
+        public Task<T> Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IQueryable<T>> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
